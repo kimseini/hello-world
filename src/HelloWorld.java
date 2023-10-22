37b09805c676b331fd4a127d9dd583ba4f55a3b8
seink <seink@KIMSEIN>
2023-10-23 03:21:35 +0900

    Hello World Commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+
+public class HelloWorld {
+	public static void main(String[] args) { // main + Ctrl + Space
+		/* Ctrl + F11 : 실행
+		 syso + Strl + Space
+		 */
+		System.out.println("Hello World"); // 줄변경 포함 출력
+		System.out.printf("%d\n",10); // \n : 줄변경
+		System.out.print("안녕"); // 줄변경 없는 출력
+		System.out.println(); // 줄변경
+	}
+}
